37323226058443ce4a51abbb98ebc7fac3748196
NikChe71 <bgum71@gmail.com>
2019-03-21 14:22:14 +0300

    Add webserver

--- webserver.py
+++ webserver.py
@@ -1,13 +1,13 @@
-import RPi.GPIO as GPIO
-import time
+#import RPi.GPIO as GPIO
+#import time
+import os
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def cube():
     return render_template('index.html')
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
