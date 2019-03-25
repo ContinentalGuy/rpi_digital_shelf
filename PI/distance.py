631ae55ada9925b1c32fb34ea9c94da16f9d75ae
NikChe71 <bgum71@gmail.com>
2019-03-25 13:41:05 +0300

    Added class DigitalShelf

--- PI/distance.py
+++ PI/distance.py
@@ -6,7 +6,7 @@ class DigitalShelf():
         # GPIO Mode (Board / BCM)
         GPIO.setmode(GPIO.BCM)
 
-        # Set GPIO pins
+        # Set 6x2 GPIO pins
         self.GPIO_TRIGGER_ECHOs = [
             (0,7),
             (5,1),
@@ -16,11 +16,30 @@ class DigitalShelf():
             (26, 21)
             ]
 
+        self.distances = []
+
     def set_direction(self):
         # Set GPIO direction (In / Out)
         for OUT, IN in GPIO_TRIGGER_ECHOs:
             GPIO.setup(IN, GPIO.IN)
             GPIO.setup(IN, GPIO.OUT)
 
-    def distance(self):
-    
+    def setup_sensors(self, trigger_pin):
+        GPIO.output(trigger_pin, GPIO.LOW)
+        print('PIN #{} is settled up'.format(trigger_pin))
+
+    def distance(self, trigger_pin, echo_pin):
+        GPIO.output(trigger, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(trigger, GPIO.LOW)
+
+        while GPIO.input(echo_pin) == 0:
+            pulse_start = time.time()
+        while GPIO.input(echo_pin) == 1:
+            pulse_stop = time.time()
+        
+        delta = pulse_stop - pulse_start
+        dist = round(delta * 17150, 2)
+        GPIO.cleanup()
+        print(dist)
+        return dist
